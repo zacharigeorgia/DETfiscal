9b3eef0f93f3c6660567a88b68bbb5b975859508
mirandatzioufa <t8240143@aueb.gr>
2025-11-19 00:41:15 +0200

    πρωτος κώδικας για το BudgetDetails

---
+++ BudgetDetails.java
@@ -0,0 +1,12 @@
+public class BudgetDetails {
+    private double inflation;
+    private double gdp;
+    private double deptRatio;
+    public double getInflation() {
+        return Inflation;
+    }
+    public double getGDP()M{
+        return gdp;
+    }
+
+}
