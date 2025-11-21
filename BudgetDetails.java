eecbc38160898bbee44a34e83812fdd79604e08e
mirandatzioufa <t8240143@aueb.gr>
2025-11-21 21:50:09 +0200

    προσθήκες στον κώδικα

--- BudgetDetails.java
+++ BudgetDetails.java
@@ -2,11 +2,23 @@ public class BudgetDetails {
     private double inflation;
     private double gdp;
     private double deptRatio;
+    public double setInflation() {
+        inflation = 2,6%;
+    }
     public double getInflation() {
         return Inflation;
     }
+    public double setGDP() {
+        gdp = 2,2;
+    }
     public double getGDP()M{
         return gdp;
+    }
+    public double setDeptRatio() {
+        deptRatio = 147,6%;
+    }
+    public double getDeptRatio() {
+        return deptRatio;
     }
     public String characterizeTotal(double balance) {
         if (balance > 0) {
