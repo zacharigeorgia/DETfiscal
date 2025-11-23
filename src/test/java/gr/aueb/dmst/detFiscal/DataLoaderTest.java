6176ce1fec302ece9d525240c67eafdc0f77bf0f
Elpida <t8240271@aueb.gr>
2025-11-23 16:17:03 +0200

    added testLoadMacrodata

--- src/test/java/gr/aueb/dmst/detFiscal/DataLoaderTest.java
+++ src/test/java/gr/aueb/dmst/detFiscal/DataLoaderTest.java
@@ -11,7 +11,7 @@ public class DataLoaderTest {
     private final String MOCK_FILE_PATH = "src/test/resources/testdata/mock_budget.json";
     // Φτιάχνουμε ένα αντικείμενο DataLoader για να το τεστάρουμε
     private DataLoader dataLoader = new DataLoader();
-    @Test
+@Test
     public void testLoadRevenues() {
         // 1. Καλούμε τη μέθοδο
         List<Revenue> revenues = dataLoader.loadRevenues(MOCK_FILE_PATH);
@@ -44,5 +44,26 @@ public class DataLoaderTest {
 
         assertFalse(e.isCanDecrease());   //Το canDecrease έπρεπε να διαβαστεί ως false από το αρχείο
         assertEquals(5.0, e.getMaxIncreasePercent()); //Το μέγιστο ποσοστό αύξησης έπρεπε να είναι 5.0
+    }
+    @Test
+    public void testLoadMacroData() {
+        // Καλούμε τη μέθοδο
+        MacroData macro = dataLoader.loadMacroData(MOCK_FILE_PATH);
+
+        // Το αντικείμενο δεν πρέπει να είναι κενό
+        assertNotNull(macro);
+
+        //  Check Summary Data (Τα γενικά σύνολα)
+        assertEquals(2000.0, macro.getTotalRevenues());
+
+        assertEquals(1000.0, macro.getTotalExpenditures());
+
+
+        assertEquals(1000.0, macro.getBudgetResult()); //Το αποτέλεσμα προϋπολογισμού έπρεπε να είναι 1000.0
+
+        //Οι παράμετροι για τα σενάρια)
+        assertEquals(24.0, macro.getVatRatePercent());
+        assertEquals(5000.0, macro.getBaseRevenueForVat());
+        assertEquals(22.0, macro.getIncomeTaxRatePercent());
     }
 }
