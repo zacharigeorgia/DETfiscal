29568bebba9ed93d806c71a9bdb6a1064c379ed0
mirandatzioufa <t8240143@aueb.gr>
2025-11-24 00:10:51 +0200

    μετακινήσεις νο 2

---
+++ src/main/java/gr/aueb/dmst/detFiscal/BudgetDetails.java
@@ -0,0 +1,32 @@
+public class BudgetDetails {
+    private double inflation;
+    private double gdp;
+    private double deptRatio;
+    public void setInflation() {
+        this.inflation = 0.026;
+    }
+    public double getInflation() {
+        return inflation;
+    }
+    public void setGDP() {
+        this.gdp = 0.022;
+    }
+    public double getGDP(){
+        return gdp;
+    }
+    public void setDeptRatio() {
+        this.deptRatio = 1.476;
+    }
+    public double getDeptRatio() {
+        return deptRatio;
+    }
+    public String characterizeTotal(double balance) {
+        if (balance > 0) {
+            return "Πλεόνασμα";
+        } else if (balance < 0) {
+            return "Έλλειμμα";
+        } else {
+            return "Ισοσκελισμένος Προϋπολογισμός";
+        }
+    }
+}
