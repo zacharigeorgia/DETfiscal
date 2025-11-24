b743110f8eaf4bb2e2708aef3e8fad22edecec02
Niki Sarafidou <t8240123@aueb.gr>
2025-11-24 17:59:21 +0200

    create window

--- src/main/java/gr/aueb/dmst/detFiscal/Menu.java
+++ src/main/java/gr/aueb/dmst/detFiscal/Menu.java
@@ -1,3 +1,13 @@
-public class Menu {
+package gr.aueb.dmst.detFiscal;
 
+import javax.swing.JFrame;
+
+public class Menu {
+    public static void main(String[] args) {
+        JFrame jf = new JFrame("Home page");
+
+        jf.setBounds(0, 0, 800, 600);
+        // Remember, the method show() is deprecated
+        jf.setVisible(true);
+    }
 }
