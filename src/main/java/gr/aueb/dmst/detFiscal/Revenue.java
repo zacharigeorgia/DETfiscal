261e46957e9ba37678773285203d1cba952318be
Marios Poluchronopoulos <t8240117@aueb.gr>
2025-11-24 16:45:31 +0200

    Modification on Revenue.java

--- src/main/java/gr/aueb/dmst/detFiscal/Revenue.java
+++ src/main/java/gr/aueb/dmst/detFiscal/Revenue.java
@@ -1,10 +1,14 @@
 public class Revenue extends Account {
     @Override
     public void increase(double x) {
-        
+        this.amount += amount;
     }
     @Override
     public void decrease(double x) {
-
+        this.amount -= amount;
+    }
+    @Override
+    public String toString() {
+        return String.format("[Έσοδο] %-40s : %,15.2f €", name, amount);
     }
 }
