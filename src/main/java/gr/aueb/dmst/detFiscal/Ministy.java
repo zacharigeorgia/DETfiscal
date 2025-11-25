47506257a5d3bc152acded8cd72affa36d288c49
Elpida <t8240271@aueb.gr>
2025-11-25 15:26:28 +0200

    added getters/setters

--- src/main/java/gr/aueb/dmst/detFiscal/Ministy.java
+++ src/main/java/gr/aueb/dmst/detFiscal/Ministy.java
@@ -4,4 +4,39 @@ public class Ministy {
     private double regularBudget;
     private double publicInvestments;
     private double total;
+
+    public String getCode() {
+         return code;
+         }
+    public void setCode(String code) {
+         this.code = code;
+          }
+
+    public String getName() {
+         return name;
+          }
+    public void setName(String name) {
+        this.name = name;
+        }
+
+    public double getRegularBudget() {
+         return regularBudget;
+         }
+    public void setRegularBudget(double regularBudget) {
+         this.regularBudget = regularBudget;
+         }
+
+    public double getPublicInvestments() {
+        return publicInvestments;
+        }
+    public void setPublicInvestments(double publicInvestments) {
+         this.publicInvestments = publicInvestments;
+         }
+
+    public double getTotal() {
+         return total;
+         }
+    public void setTotal(double total) {
+         this.total = total;
+         }
 }
