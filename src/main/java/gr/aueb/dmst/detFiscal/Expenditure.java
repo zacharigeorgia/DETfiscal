2832a1b8b7af09ade92645aecd645b4bf8be1490
Marios Poluchronopoulos <t8240117@aueb.gr>
2025-12-01 20:54:00 +0200

    hopefully adding the files to develop

--- src/main/java/gr/aueb/dmst/detFiscal/Expenditure.java
+++ src/main/java/gr/aueb/dmst/detFiscal/Expenditure.java
@@ -1,36 +1,44 @@
 package gr.aueb.dmst.detFiscal;
 
-public class Expenditure {
-    private String name;
-    private double amount;
+public class Expenditure extends Account {
     private boolean canDecrease;
     private double maxIncreasePercent;
 
-    // Setters
-    public void setName(String name) {
-        this.name = name;
-        }
-    public void setAmount(double amount) {
-        this.amount = amount;
-        }
+    // Setters (για τον DataLoader)
     public void setCanDecrease(boolean canDecrease) {
-         this.canDecrease = canDecrease;
-         }
+        this.canDecrease = canDecrease;
+    }
     public void setMaxIncreasePercent(double maxIncreasePercent) {
         this.maxIncreasePercent = maxIncreasePercent;
-         }
+    }
 
     // Getters
-    public String getName() {
-        return name;
-        }
-    public double getAmount() {
-         return amount;
-          }
-    public boolean isCanDecrease() {
-         return canDecrease;
-         }
+    public boolean isCanDecrease() { 
+        return canDecrease;
+    }
     public double getMaxIncreasePercent() {
         return maxIncreasePercent;
+    }
+
+    @Override
+    public void increase(double amount) {
+        // Εδώ θα μπει αργότερα ο έλεγχος του maxIncreasePercent
+        if () {
+            this.amount += amount;
         }
+    }
+
+    @Override
+    public void decrease(double amount) {
+        // Εδώ θα μπει αργότερα ο έλεγχος του canDecrease
+        if () {
+            this.amount -= amount;
+        }
+    }
+
+    @Override
+    public String toString() {
+        String lockStatus = canDecrease ? "" : " [ΚΛΕΙΔΩΜΕΝΟ]";
+        return String.format("[Έξοδο] %-40s : %,15.2f € %s", name, amount, lockStatus);
+    }
 }
--- src/main/java/gr/aueb/dmst/detFiscal/Revenue.java
+++ src/main/java/gr/aueb/dmst/detFiscal/Revenue.java
@@ -1,14 +1,14 @@
-package gr.aueb.dmst.detFiscal;
-
-public class Revenue {
-    private String name;
-    private double amount;
-
-    // Setters (Τους χρειάζεται ο DataLoader)
-    public void setName(String name) { this.name = name; }
-    public void setAmount(double amount) { this.amount = amount; }
-
-    // Getters (Τους χρειάζεται το Test για τα asserts)
-    public String getName() { return name; }
-    public double getAmount() { return amount; }
+public class Revenue extends Account {
+    @Override
+    public void increase(double x) {
+        this.amount += amount;
+    }
+    @Override
+    public void decrease(double x) {
+        this.amount -= amount;
+    }
+    @Override
+    public String toString() {
+        return String.format("[Έσοδο] %-40s : %,15.2f €", name, amount);
+    }
 }
