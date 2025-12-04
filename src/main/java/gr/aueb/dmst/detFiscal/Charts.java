2f7666838cd62ae12d674f1dca2e72840834f1d4
t8240003 <t8240003@aueb.gr>
2025-12-04 12:00:34 +0200

    Creation of class for Charts and some imports

---
+++ src/main/java/gr/aueb/dmst/detFiscal/Charts.java
@@ -0,0 +1,11 @@
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import javax.swing.JFrame;
+import java.awt.BorderLayout;
+
+public class Charts {
+    
+}
