bd59abcace3ce5e17990d6f39e226187aeb216e5
t8240003 <t8240003@aueb.gr>
2025-12-08 17:28:16 +0200

    First data Charts.java will need

--- src/main/java/gr/aueb/dmst/detFiscal/Charts.java
+++ src/main/java/gr/aueb/dmst/detFiscal/Charts.java
@@ -1,3 +1,5 @@
+package gr.aueb.dmst.detfiscal;
+
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
@@ -7,5 +9,26 @@ import javax.swing.JFrame;
 import java.awt.BorderLayout;
 
 public class Charts {
-    
+    private DefaultCategoryDataset createComparisonDataset(
+        FederalBudget budget1, FederalBudget budget2, String categoryName1, String categoryName2)
+        /*Δέχεται τον 1ο προϋπολογισμό, μετά τον 2ο και μετά ονόματα κατηγορίας έτος ή χώρα */
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        
+        //Data για πρώτο αντικείμενο
+        dataset.addValue(budget1.getSummary().calculateTotalRevenues(),
+                         "Σύνολο Εσόδων", categoryName1);
+        dataset.addValue(budget1.getSummary().calculateTotalExpenditures(),
+                         "Σύνολο Εξόδων", categoryName1);
+        dataset.addValue(budget1.calculateTotalBudget(),
+                         "Ισοζύγιο", categoryName1);
+
+        //Data για δεύτερο αντικείμενο
+        dataset.addValue(budget2.getSummary().calculateTotalRevenues(),
+                         "Σύνολο Εσόδων", categoryName2);
+        dataset.addValue(budget2.getSummary().calculateTotalExpenditures(),
+                         "Σύνολο Εξόδων", categoryName2)
+        dataset.addValue(budget2.getSummary().calculateTotalBudget(),
+                         "Ισοζύγιο", categoryName2);
+        
+        return dataset;
 }
