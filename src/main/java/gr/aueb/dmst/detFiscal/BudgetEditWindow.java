b32322c164c879d5eec8ffce00ddc6bf7154a2a7
Niki Sarafidou <t8240123@aueb.gr>
2025-12-27 14:15:36 +0200

    βασική δομή παραθύρου

--- src/main/java/gr/aueb/dmst/detFiscal/BudgetEditWindow.java
+++ src/main/java/gr/aueb/dmst/detFiscal/BudgetEditWindow.java
@@ -3,6 +3,16 @@ package gr.aueb.dmst.detFiscal;
 import javax.swing.*;
 import java.awt.*;
 
-public class BudgetEditWindow {
+public class BudgetEditWindow extends JFrame {
 
+    public BudgetEditWindow(FederalBudget fedBudget, ChangeLog changeLog) {
+
+        // Βασικές ρυθμίσεις παραθύρου
+        setTitle("Επεξεργασία Δεδομένων");
+        setSize(400, 300);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        add(new JLabel("...", SwingConstants.CENTER));
+    }
 }
